32906088b082dbad1796bef11364e6acfd4f4298
incubatedink <j.king.hou+incubatedink@gmail.com>
2017-04-03 02:04:14 -0700

    Added quicksort to recursion.erl

--- recursion.erl
+++ recursion.erl
@@ -5,3 +5,19 @@ lenient_zip(A, B) -> lenient_zip(A, B, []).
 lenient_zip([], _, A) -> A;
 lenient_zip(_, [], A) -> A;
 lenient_zip([X|Xs],[Y|Ys], Acc) -> lenient_zip(Xs, Ys, Acc++[{X, Y}]).
+
+quicksort([]) -> [];
+quicksort(A = [_]) -> A;
+quicksort([Pivot|List]) -> 
+	{L, R} = split_by_pivot(Pivot, List),
+	quicksort(L) ++ [Pivot] ++ quicksort(R).
+
+split_by_pivot(P, L) -> split_by_pivot(P, L, [], []).
+split_by_pivot(_, [], L, R) -> {L,R};
+split_by_pivot(P, [H|T], L, R) ->
+	if
+		H =< P ->
+			split_by_pivot(P, T, L++[H], R);
+		H > P ->
+			split_by_pivot(P, T, L, R++[H])
+		end.
